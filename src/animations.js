3eb7ac7ac8d593de402fe7c792f857b925ec70cd
madcampos <609904+madcampos@users.noreply.github.com>
2026-09-21 01:14:12 -0400

    feat: add animations notes

---
+++ src/animations.js
@@ -0,0 +1,10 @@
+/**
+ * Elements with attributes `data-animate-in`, `data-animate-highglight`, `data-animate-out` will animate.
+ * Animations have `data-animate-*-duration` properties for duration, and `data-animate*-delay` for delay (those can be inherited from parent elements).
+ *
+ * Use CSS' new typed attribute function to handle duration and delay.
+ *
+ * Add animation stylesheet and dynamically load it based on presence of data attributes.
+ *
+ * Add transitions stylesheet based on slide transitions. Use view transition API?
+ */
